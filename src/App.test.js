9f0c64338a251bbacbd6a6f9ca9cef157d8f90f0
kirkby <kirkby@neighbor.com>
2021-02-09 21:08:04 -0700

    progress

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import App from './App';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 test("App renders", () => {
   render(<App />);
+})
+
+test('pushing button gets and renders missions data', () => {
+  render(<App />);
+
+  const btn = screen.getByRole("button");
+
+  userEvent.click(btn);
+
+  waitFor(() => screen.getAllByTestId("mission"))
 })
