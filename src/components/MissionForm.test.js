98f381107e0fbf3375d44e8504c6db2ce880d393
kirkby <kirkby@neighbor.com>
2021-02-09 19:56:30 -0700

    progress

---
+++ src/components/MissionForm.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import MissionForm from './MissionForm';
+import { render } from '@testing-library/react';
+
+test('MissionForm renders', () => {
+  render(<MissionForm />)
+})
