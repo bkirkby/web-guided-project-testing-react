31128ff7b53cbd7f9ea0b1bc3afc1fc388038716
kirkby <kirkby@neighbor.com>
2021-02-09 21:13:12 -0700

    progress

--- src/App.test.js
+++ src/App.test.js
@@ -17,4 +17,6 @@ test('pushing button gets and renders missions data', async () => {
   const missions = await waitFor(() => screen.getAllByTestId("mission"))
 
   console.log("bk: missions: ", missions);
+
+  expect(missions).toHaveLength(10);
 })
