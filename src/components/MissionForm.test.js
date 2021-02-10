1eff5815debddc8749abe502cfa75b1b05d1cdf6
kirkby <kirkby@neighbor.com>
2021-02-09 20:04:49 -0700

    progress

--- src/components/MissionForm.test.js
+++ src/components/MissionForm.test.js
@@ -4,4 +4,10 @@ import { render } from '@testing-library/react';
 
 test('MissionForm renders', () => {
   render(<MissionForm />)
+});
+
+test('renders properly when fetchingData', () => {
+  render(<MissionForm isFetchingData={true} />)
+
+
 })
