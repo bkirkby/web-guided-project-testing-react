7fb599496922a51be6c0a964a1cc46858bdb9046
kirkby <kirkby@neighbor.com>
2021-02-09 20:50:34 -0700

    progress

--- src/components/MissionForm.test.js
+++ src/components/MissionForm.test.js
@@ -28,7 +28,7 @@ test('getData is called when button is pressed', () => {
   // const btn = screen.getByRole("button");
   const btn = screen.getByText(/get data/i);
 
-  expect(() => userEvent.click(btn)).not.toThrow();
+  expect(() => userEvent.click(btn)).not.toThrowError();
 
   expect(mockGetData).toBeCalledTimes(1);
 })
