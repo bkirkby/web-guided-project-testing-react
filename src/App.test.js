632a7ea8cf7cbe4d040e6410d1425280ef6fb33b
kirkby <kirkby@neighbor.com>
2021-04-06 21:17:06 -0600

    progress

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 import mockFetchMissions from './api/fetchMissions';
 import { missionsFixture } from './components/MissionsList';
+import userEvent from '@testing-library/user-event';
 
 jest.mock('./api/fetchMissions');
 
@@ -13,8 +14,13 @@ test('App renders', () => {
 test('fetches and renders mission data', async () => {
   render(<App />);
   mockFetchMissions.mockResolveValueOnce({
-    data: [
+    data: missionsFixture
+  })
 
-    ]
+  const btn = screen.getByRole("button");
+  userEvent.click(btn);
+
+  await waitFor(() => {
+    screen.getAllByTestId("mission").toHaveLength(2)
   })
 })
