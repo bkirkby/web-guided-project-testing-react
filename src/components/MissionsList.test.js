522c1b405d8065a42d7a222266e81f24117df94c
kirkby <kirkby@neighbor.com>
2021-04-06 21:01:34 -0600

    progress

--- src/components/MissionsList.test.js
+++ src/components/MissionsList.test.js
@@ -2,6 +2,21 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import MissionsList from './MissionsList';
 
+const missionsFixture = [
+  {
+    mission_name: 'bks trip to mars',
+    mission_id: 'marsbk1'
+  },
+  {
+    mission_name: 'bks trip to venus',
+    mission_id: 'venusbk1'
+  }
+]
+
 test('MissionsList renders', () => {
-  render(<MissionsList missions={[]} />)
+  const { rerender } = render(<MissionsList missions={[]} />)
+
+  let missionObjects = screen.queryAllTestId("mission");
+  expect(missionObjects).toHaveLength(0);
+
 })
