231c44243a035b99ff8e3f59dc9767d9a9deb8c3
kirkby <kirkby@neighbor.com>
2021-04-06 20:00:37 -0600

    progress

--- src/components/MissionForm.test.js
+++ src/components/MissionForm.test.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import MissionForm from './MissionForm';
 
-describe('missionform tests', () => {
-  it('renders MissionForm', () => {
+// describe('missionform tests', () => {
+//   it('renders MissionForm', ()=> {
 
-  })
-  // test('', ()=>{
-  // })
+//   })
+// })
+
+test('MissionForm renders', () => {
 })
