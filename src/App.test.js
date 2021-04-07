405f8a61bd25e37735779b18cc39cb9e30f5f7ca
kirkby <kirkby@neighbor.com>
2021-04-06 21:14:03 -0600

    progress

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import mockFetchMissions from './api/fetchMissions';
+import { missionsFixture } from './components/MissionsList';
 
 jest.mock('./api/fetchMissions');
 
@@ -11,5 +12,9 @@ test('App renders', () => {
 
 test('fetches and renders mission data', async () => {
   render(<App />);
+  mockFetchMissions.mockResolveValueOnce({
+    data: [
 
+    ]
+  })
 })
