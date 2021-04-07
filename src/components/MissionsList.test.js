cb669c7fdb7a4560fd7f12062fb252ed3d1d9ceb
kirkby <kirkby@neighbor.com>
2021-04-06 20:55:29 -0600

    progress

---
+++ src/components/MissionsList.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import MissionsList from './MissionsList';
+
+test('MissionsList renders', () => {
+  render(<MissionsList />)
+})
