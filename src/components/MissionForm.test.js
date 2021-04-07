0114f939ec39cdb6a738a56a5731e1764438f13d
kirkby <kirkby@neighbor.com>
2021-04-06 20:39:01 -0600

    progress

--- src/components/MissionForm.test.js
+++ src/components/MissionForm.test.js
@@ -15,19 +15,22 @@ test('MissionForm renders', () => {
 
 test('MissionForm renders button if not fetching data', () => {
   render(<MissionForm isFetchingData={false} />)
-
   const btn = screen.queryByText(/get datae/i);
-
   expect(btn).toBeNull();
   // const btn = screen.getByRole("button");
 })
 
 test('MissionForm renders message if fetching data', () => {
   render(<MissionForm isFetchingData={true} />);
-
   const message = screen.getByText(/we are fetching data/i);
   const btn = screen.queryByText(/get data/i);
-
   expect(message).toBeDefined();
   expect(btn).toBeNull();
+})
+
+test('getData is called when button is pressed', () => {
+  const mockGetData = jest.fn(() => { })
+  render(<MissionForm isFetchingData={false} getData={mockGetData} />)
+
+  expect(mockGetData.mock.calls === 1);
 })
