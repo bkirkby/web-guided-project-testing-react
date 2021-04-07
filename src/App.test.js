945ee2bec0f622a29318d663ddf41277bc72c04c
kirkby <kirkby@neighbor.com>
2021-04-06 21:10:37 -0600

    progress

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import mockFetchMissions from './api/fetchMissions';
 
 test('App renders', () => {
   render(<App />);
+});
+
+test('fetches and renders mission data', async () => {
+  render(<App />);
+
 })
