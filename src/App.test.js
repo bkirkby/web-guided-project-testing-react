abbc92794b189cca9f9188edf5011a6ef07aa9a3
kirkby <kirkby@neighbor.com>
2021-04-06 21:23:48 -0600

    progress

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen, waitFor, wait } from '@testing-library/react';
 import App from './App';
-import mockFetchMissions from './api/fetchMissions';
+import { fetchMissions as mockFetchMissions } from './api/fetchMissions';
 import { missionsFixture } from './components/MissionsList';
 import userEvent from '@testing-library/user-event';
 
